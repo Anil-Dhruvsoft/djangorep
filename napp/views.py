d78a707099d8e6ebace903ec84237ab992096f07
Anil Thanneru <56641929+Anil0612@users.noreply.github.com>
2019-10-18 15:50:45 +0530

    Update views.py

--- napp/views.py
+++ napp/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def homepage(request):
-	return HttpResponse('Hello World, I am a Django application. Created by Anil')
+	return HttpResponse('Hello World, This is the file diployed to the github')
 
