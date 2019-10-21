038895d7e9b9cb7c816b450f509c326f969729f9
Anil Dhruvsoft <56641929+Anil0612@users.noreply.github.com>
2019-10-21 15:13:36 +0530

    Update views.py

--- napp/views.py
+++ napp/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def homepage(request):
-	return HttpResponse(<h1>This application is now enabled with auto deployments</h1>)
+	return render(request,'home.html')
 
