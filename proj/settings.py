bb71aa5711efed9e70bd514423ec57dff4eb9216
Anil Dhruvsoft <56641929+Anil0612@users.noreply.github.com>
2019-10-21 15:18:58 +0530

    Update settings.py

--- proj/settings.py
+++ proj/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(Base_Dir,'template')],
+        'DIRS': [os.path.join(Base_Dir,'pages')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
